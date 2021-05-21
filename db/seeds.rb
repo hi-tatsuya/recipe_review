91106297f876f254877701ddf13c46fa5e1b6e48
hiyamatatsuya <tatsuyashonan@gmail.com>
2021-05-21 10:46:16 +0900

    レシピ一覧画面作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create(name:"至高のチャーハン",maker:"リュウジのバズレシピ")
+Recipe.create(name:"至高のナポリタン",maker:"リュウジのバズレシピ")
+Recipe.create(name:"サバ缶の味噌煮",maker:"コウケンテツ")
+Recipe.create(name:"八宝菜",maker:"クラシル")
