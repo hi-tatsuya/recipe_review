a7bd1d6445f136573ecdd86246dffe98017c486a
hiyamatatsuya <tatsuyashonan@gmail.com>
2021-06-20 15:27:02 +0900

    carrer_wave修正

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,7 +7,7 @@ if Rails.env.production?
       region: "ap-northeast-1"
     }
     config.fog_directory = "recipe-review-bk"
-    config.asset_host = "static.recipe-review.com"
+    config.asset_host = "https://static.recipe-review.com"
 
   end
 end
